88f352e001fb89ee638f12297dff9ae1c50a7862
M Brabu <brabumurugan18@gmail.com>
2026-09-19 13:42:06 +0530

    upadte 19-9-2026

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,14 +4,14 @@ function Navbar(){
     return(
        <>
        <div className="Navbar">
-        <h1 className='m-2'> <img src={airplane} id="airplanelogo" className='mx-2 '/> Skywings Data Analysis</h1>
-        <div className='btn-group p-3'>
+        <h1 className='m-2 '> <img src={airplane} id="airplanelogo" className='mx-2 ' /> Skywings Data Analysis</h1>
+        <div className='btngroup p-3'>
         <Link to="/" className='btn btn-primary '>Home</Link>    
-        <Link to="/Report" className='btn btn-dark '>Report</Link>
-        <Link to="/Dashboard" className='btn btn-light'>Dashboard</Link>
-        <Link to="/Dataset" className='btn Dbbutton'>Dataset</Link>
-        <Link to="/Story" className='btn btn-warning'>Story</Link>
-        <Link to="/Exploration" className='btn btn-success'>Exploration</Link>
+        <Link to="/Report" className='btn btn-primary '>Report</Link>
+        <Link to="/Dashboard" className='btn btn-primary'>Dashboard</Link>
+        <Link to="/Dataset" className='btn btn-primary'>Dataset</Link>
+        <Link to="/Story" className='btn btn-primary'>Story</Link>
+        <Link to="/Exploration" className='btn btn-primary'>Exploration</Link>
        </div>
        </div>
        </> 
